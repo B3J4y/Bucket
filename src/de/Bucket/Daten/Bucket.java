afb399ed50f407c7e9f6f9fb15db99ef1807d8df
Jan Bernoth <janbernoth@gmail.com>
2013-09-01 13:38:16 +0200

    Final

--- src/de/Bucket/Daten/Bucket.java
+++ src/de/Bucket/Daten/Bucket.java
@@ -11,11 +11,19 @@ public class Bucket {
 	private List<Klausel> klauseln;
 	private int smallestNumber;
 	
+	/**
+	 * Konstruktur der ein leeres Bucket initialisiert. 
+	 * @param numb
+	 */
 	public Bucket(int numb){
 		smallestNumber = numb;
 		klauseln = new ArrayList<Klausel>();
 	}
 
+	/**
+	 * Konstruktur, der gleich eine Klausel in das Bucket initialisiert
+	 * @param kls - Klausel
+	 */
 	public Bucket(Klausel kls){
 		if(kls.getVars().size() > 0){
 			smallestNumber = kls.getSmallestNumbber();
@@ -24,6 +32,12 @@ public class Bucket {
 		}
 	}
 	
+	
+	/**
+	 * Fügt eine Klausel zu dem Bucket hinzu
+	 * @param kls - Klausel
+	 * @return Boolean, über den Erfolg des Vorgangs
+	 */
 	public boolean addKlausel(Klausel kls){
 		if(kls.getSmallestNumbber() == smallestNumber){
 			klauseln.add(kls);
@@ -31,16 +45,27 @@ public class Bucket {
 		} else {
 			return false;
 		}
-}
-	
+	}
+	/**
+	 * Getter
+	 * @return
+	 */
 	public List<Klausel> getKlauseln() {
 		return klauseln;
 	}
-
+	
+	/**
+	 * Setter
+	 * @param vars
+	 */
 	public void setKlauseln(List<Klausel> klauseln) {
 		this.klauseln = klauseln;
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public int getSmallestNumber() {
 		return smallestNumber;
 	}
--- src/de/Bucket/Daten/Klausel.java
+++ src/de/Bucket/Daten/Klausel.java
@@ -11,15 +11,32 @@ public class Klausel {
 	private List<Integer> vars;
 	private Integer high;
 	
+	/**
+	 * Konstruktor der die Klausel mit der höchsten  Zahl und einer Liste von
+	 * von Variablen erzeugt.
+	 * @param high - Höchste mögliche Zahl
+	 * @param vars - List der Variablen
+	 */
 	public Klausel(int high, List<Integer> vars) {
 		this.setVars(vars);
 		this.setHigh(high);
 	}
+	
+	/**
+	 * Initialisiert eine leere Klausel
+	 * @param high - Höchste mögliche Zahl
+	 */
 	public Klausel(int high) {
 		this.setVars(new ArrayList<Integer>());
 		this.setHigh(high);
 	}
 	
+	
+	/**
+	 * Fügt eine Variable hinzu
+	 * @param numb - neue Zahl
+	 * @return Boolean, ob der Vorgang erfolgreich war
+	 */
 	public Boolean addVar(int numb){
 		if(numb <= high){
 			vars.add(numb);
@@ -29,6 +46,10 @@ public class Klausel {
 		}
 	}
 	
+	/**
+	 * wandelt die Liste von Vars in einen String um.
+	 * @return String mit allen Variablen
+	 */
 	public String varsToString(){
 		String str = "{";
 		for(Integer i : vars){
@@ -40,6 +61,10 @@ public class Klausel {
 		return str + "}";
 	}
 	
+	/**
+	 * rechnet die kleinste Zahl der Liste aus
+	 * @return Integer - kleinste Zahl aus der List
+	 */
 	public int getSmallestNumbber(){
 		if(vars.size() == 0){
 			return 0;
@@ -54,18 +79,33 @@ public class Klausel {
 		}
 	}
 	
+	/**
+	 * Getter
+	 * @return
+	 */
 	public List<Integer> getVars() {
 		return vars;
 	}
-
+	/**
+	 * Setter
+	 * @param vars
+	 */
 	public void setVars(List<Integer> vars) {
 		this.vars = vars;
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public Integer getHigh() {
 		return high;
 	}
 
+	/**
+	 * Setter
+	 * @param vars
+	 */
 	public void setHigh(Integer high) {
 		this.high = high;
 	}
